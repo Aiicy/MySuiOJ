8b297468aac5bc40be3ee3dd7bb07e262979257a
airicy <gongjinqi@qq.com>
2016-10-15 23:11:59 +0800

    update svg

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:80/';/**/
+$config['base_url'] = 'http://localhost:80/CodeIgniter/MySui/';/**/
 
 /*
 |--------------------------------------------------------------------------
